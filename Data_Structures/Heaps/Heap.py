de2586643b373d3e5a919a8c4e38924175b5d1e9
Kenneth <kcheung331@gmail.com>
2019-08-07 23:15:33 -0700

    fix heap

--- Data_Structures/Heaps/Heap.py
+++ Data_Structures/Heaps/Heap.py
@@ -1,42 +1,46 @@
 
 class MinHeap():
-	def __init__(self):
-		self.heap = []
-		self.currentSize = 0
+    def __init__(self):
+        self.heap = []
+        self.currentSize = 0
 
-	def heapPush(self, value):
-		self.heap.append(value)
-		self.currentSize += 1
-		self.heapifyUp(self.currentSize - 1)
-		pass
+    def heapPush(self, value):
+        self.heap.append(value)
+        self.currentSize += 1
+        self.heapifyUp(self.currentSize - 1)
 
-	def heapPop(self):
-		pop = self.heap[0]
-		self.heap[0] , self.heap[self.currentSize - 1] = self.heap[self.currentSize - 1], self.heap[0]
-		self.heapifyDown(0)
+    def __swap(self, a, b):
+        self.heap[a], self.heap[b] = self.heap[b], self.heap[a]
 
-	def heapifyUp(self, i):
-		parent = (i-1) // 2
-		while parent >= 0:
-			if self.heap[i] < self.heap[parent]:
-				self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
-			parent = (parent - 1) // 2
+    def heapPop(self):
+        pop = self.heap[0]
+        self.__swap(0, self.currentSize - 1)
+        self.currentSize -= 1
+        self.heapifyDown(0)
+        return pop
 
-	def getSmallerChild(self, i):
-		if 2 * i + 2 > self.currentSize - 1:
-			return 2 * i + 1
-		else:
-			if self.heap[2 * i + 1] < self.heap[2 * i + 2]:
-				return 2 * i + 1
-			else:
-				return 2 * i + 2
+    def heapifyUp(self, i):
+        parent = (i-1) // 2
+        while parent >= 0:
+            if self.heap[i] < self.heap[parent]:
+                self.__swap(i, parent)
+            i = parent
+            parent = (parent - 1) // 2
 
-	def heapifyDown(self, i):
-		while 2 * i + 1 <= self.currentSize - 1:
-			smaller = self.getSmallerChild(i)
-			if self.heap[smaller] < self.heap[i]:
-				self.heap[smaller], self.heap[i] = self.heap[i], self.heap[smaller]
-			i = smaller
+    def getSmallerChild(self, i):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if right > self.currentSize - 1:
+            return left
+        else:
+            return left if self.heap[left] < self.heap[right] else right
+
+    def heapifyDown(self, i):
+        while 2 * i + 1 <= self.currentSize - 1:
+            smaller = self.getSmallerChild(i)
+            if self.heap[smaller] < self.heap[i]:
+                self.__swap(smaller, i)
+            i = smaller
 
 
 h = MinHeap()
@@ -46,8 +50,9 @@ h.heapPush(7)
 h.heapPush(23)
 h.heapPush(2)
 h.heapPush(12)
+h.heapPush(float("-inf"))
 h.heapPush(22)
 h.heapPush(25)
-h.heapPop()
-
+for i in range(h.currentSize):
+    print(h.heapPop())
 print(h.heap)
